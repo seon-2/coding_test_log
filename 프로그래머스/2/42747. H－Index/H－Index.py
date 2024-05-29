8e4aa93022b56dfd3ac5697b1e74f5e04bf704c1
seon-2 <73648391+seon-2@users.noreply.github.com>
2024-05-29 09:19:35 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,18 +1,19 @@
-def solution(citations):
-    c_len = len(citations)
-    answer = 0
-    
-    # 정렬
-    c_sorted = sorted(citations)
-    
-    for i in range(c_len+1, 0, -1):
-        if c_len >= i and c_sorted[c_len - i] >= i:
-            answer = i
-            break
-    
-    return answer
+# 시간복잡도 개선
 
-# [0, 1, 3, 5, 6]
-# 5편의 논문 중 3번이상 인용된 논문이 3편 이상이고 나머지 논문이 3번 이하 인용되었다면 h-index가 3
-# 일단 0을 다 빼야할까
-# 최대값도 len이고 정답도 index?
+def solution(citations):
+    n = len(citations)
+    max_val = max(citations)
+    count = [0] * (max_val + 1)
+
+    # 각 인용 횟수에 해당하는 논문 수 계산
+    for c in citations:
+        count[c] += 1
+
+    total = 0
+    
+    for i in range(max_val, -1, -1):
+        total += count[i]
+        if total >= i:
+            return i
+
+    return 0
--- 프로그래머스/2/42747. H－Index/README.md
+++ 프로그래머스/2/42747. H－Index/README.md
@@ -1,14 +1,14 @@
 # [level 2] H-Index - 42747 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.00 ms
+메모리: 10.3 MB, 시간: 0.01 ms
 
 ### 구분
 
- > 
+코딩테스트 연습 > 정렬
 
 ### 채점결과
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 27일 22:02:47
+2024년 05월 29일 09:19:33
 
 ### 문제 설명
 
