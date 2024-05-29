abbe3b474c76f22e24fc9238a0e7e8e503789ce8
seon-2 <73648391+seon-2@users.noreply.github.com>
2024-05-29 09:27:18 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,3 +1,5 @@
+# getDivisior 함수 개선
+
 def solution(brown, yellow):
     
     def getAnswer(w, h):
@@ -7,20 +9,11 @@ def solution(brown, yellow):
             return False
         
     def getDivisior(n):
-        d_list = []
-
-        for i in range(1, int(n**(1/2)) + 1):
-            temp = []
+        divisors = []
+        for i in range(1, int(n**0.5) + 1):
             if n % i == 0:
-                if (i**2) != n:
-                    temp.append(n // i)
-                    temp.append(i)
-                else:
-                    temp.append(i)
-                    temp.append(i)
-                d_list.append(temp)
-
-        return d_list
+                divisors.append((n // i, i))
+        return divisors
 
     d_list = getDivisior(brown+yellow)
     
@@ -30,10 +23,3 @@ def solution(brown, yellow):
         if getAnswer(d[0], d[1]):
             return d
 
-
-# b+y 가 w*h인지 찾기
-# 카펫이 되는 경우가 뭘까
-# 48의 약수는 (1, 48) (2, 24) (3, 16) (4, 12) (6, 8) 인데
-# 무조건 1이 포함되어있으면 안되고, 2도 안되네. 갈색이 테두리여야 하니까.
-# 테두리(b)의 개수는 2(w+h) - 4 : 모서리 제외
-# 이런 조건을 만족하는 거 찾기
