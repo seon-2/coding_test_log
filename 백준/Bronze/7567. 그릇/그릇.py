9ec7cdb86ae1269ecf4c56890a4a7b810c47e49d
seon-2 <73648391+seon-2@users.noreply.github.com>
2024-06-05 16:56:01 +0900

    [Bronze II] Title: 그릇, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/7567. 그릇/그릇.py
+++ 백준/Bronze/7567. 그릇/그릇.py
@@ -1,25 +1,16 @@
-# ---- 4번 그릇
-# 왼쪽부터 하나씩 빼면서 처음에는 +10, 같으면 +5, 다르면 +10
-# 왼쪽부터 해야하니 queue 사용
-
 import sys
-from collections import deque
-# sys.stdin = open("input.txt","rt")
-
-data = list(sys.stdin.readline().strip())
-plates = deque()
-for plate in data:
-    plates.append(plate)
+# sys.stdin = open("input.txt", "rt")
+data = sys.stdin.readline().strip()
 
 height = 10
-flag = plates.popleft()
 
-while len(plates):
-    now = plates.popleft()
-    if flag == now:
+previous_plate = data[0]
+
+for current_plate in data[1:]:
+    if previous_plate == current_plate:
         height += 5
     else:
         height += 10
-    flag = now
+    previous_plate = current_plate
 
 print(height)
