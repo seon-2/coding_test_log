b1a8c41af828a97f74e15f50ef22d13c86df04c0
seon-2 <73648391+seon-2@users.noreply.github.com>
2024-06-05 16:58:02 +0900

    [Silver III] Title: 프린터 큐, Time: 72 ms, Memory: 34044 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.py
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -1,41 +1,33 @@
-# ---- 5번 프린터 큐
-
 import sys
 from collections import deque
 
-# sys.stdin = open("input.txt","rt")
-data = sys.stdin.readline
-test_case_num = int(data())
-test_case = []
+input = sys.stdin.read
+data = input().split()
+index = 0
+
+test_case_num = int(data[index])
+index += 1
 answers = []
-def printer(N, M, list):
-    imp_queue = deque()
-    for i in range(N):
-        imp_queue.append([i, list[i]])
 
-    # print("imp_queue[M] : ", imp_queue[M])
-
-    print_seq = {}
+def printer(N, M, priorities):
+    imp_queue = deque((i, priorities[i]) for i in range(N))
     count = 0
 
-    while len(imp_queue):
+    while imp_queue:
         flag = imp_queue.popleft()
-        if len(imp_queue) and flag[1] < max(imp_queue, key=lambda x:x[1])[1]: # 나보다 중요도 높은 문서가 있다면
+        if any(flag[1] < item[1] for item in imp_queue):  # 나보다 중요도 높은 문서가 있다면
             imp_queue.append(flag)
         else:
             count += 1
-            print_seq[flag[0]] = count
-
-    # M번째 문서가 몇 번째로 출력되는지 확인
-    # print(f"M 번째 문서 (index {M}) 는 {print_seq[M]} 번째로 출력됩니다.")
-    answers.append(print_seq[M])
+            if flag[0] == M:
+                return count
 
 for _ in range(test_case_num):
-    test_case.append(list(map(int, data().strip().split())))
-    test_case.append(list(map(int, data().strip().split())))
+    N = int(data[index])
+    M = int(data[index + 1])
+    index += 2
+    priorities = list(map(int, data[index:index + N]))
+    index += N
+    answers.append(printer(N, M, priorities))
 
-for n in range(test_case_num):
-    printer(test_case[n*2][0], test_case[n*2][1], test_case[n*2+1])
-
-# print("\n".join(answers))
 print(*answers, sep="\n")
