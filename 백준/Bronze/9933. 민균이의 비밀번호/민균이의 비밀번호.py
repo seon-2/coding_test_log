5d7f1160a0c17d3e8827edf609f94a8dbe0e60c0
seon-2 <73648391+seon-2@users.noreply.github.com>
2024-06-06 09:56:54 +0900

    [Bronze I] Title: 민균이의 비밀번호, Time: 48 ms, Memory: 33188 KB -BaekjoonHub

---
+++ 백준/Bronze/9933. 민균이의 비밀번호/민균이의 비밀번호.py
@@ -0,0 +1,17 @@
+# reverse 단어도 포함되어 있는 단어 찾기
+
+import sys
+from heapq import *
+# sys.stdin = open("input.txt", "rt")
+data = sys.stdin.readline
+
+N = int(data())
+words = [data().strip() for _ in range(N)]
+
+heapify(words)
+
+while words:
+    word = heappop(words)
+    reversed_word = word[::-1]
+    if word == reversed_word or reversed_word in words:
+        print(len(word), word[len(word) // 2])
