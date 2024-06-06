da06b4b93e208d450062125d08e086f91db3afd5
seon-2 <73648391+seon-2@users.noreply.github.com>
2024-06-06 15:23:58 +0900

    [Bronze II] Title: Hashing, Time: 48 ms, Memory: 31252 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -1,5 +1,4 @@
 import sys
-# sys.stdin = open("input.txt", "rt")
 data = sys.stdin.readline
 
 L = int(data())
@@ -7,16 +6,26 @@ input_data = data().strip()
 mod_number = 1234567891
 
 
-def make_mod_pow(l):
+def modular_pow(base, exponent, modulus):
+    result = 1
+    base %= modulus
 
+    while exponent > 0:
+        if exponent % 2 == 1:
+            result = (result * base) % modulus
+        exponent //= 2
+        base = (base * base) % modulus
+
+    return result
+
+
+def make_mod_pow(l, mod_number):
     mod_pow = [0 for _ in range(l)]
-
     mod_pow[0] = 1
-    mod_pow[1] = 31
-    mod_pow[2] = (31 ** 2) % mod_number
+    base = 31
 
-    for i in range(3, l):
-        mod_pow[i] = (mod_pow[i-1] * mod_pow[1]) % mod_number
+    for i in range(1, l):
+        mod_pow[i] = modular_pow(base, i, mod_number)
 
     return mod_pow
 
@@ -35,6 +44,6 @@ def get_hash(mod_pow, num_list):
     return answer % mod_number
 
 
-mod_pow = make_mod_pow(L)
+mod_pow = make_mod_pow(L, mod_number)
 num_list = make_num_list(input_data)
 print(get_hash(mod_pow, num_list))
