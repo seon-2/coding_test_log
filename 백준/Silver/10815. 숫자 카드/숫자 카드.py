7c18be13c7270753214c7109d6c272b55c5cd06e
seon-2 <73648391+seon-2@users.noreply.github.com>
2024-06-07 21:39:39 +0900

    [Silver V] Title: 숫자 카드, Time: 592 ms, Memory: 125184 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,20 +1,12 @@
-# 8번 : 숫자카드
-# 상근이가 가진 숫자카드, 중복X
-# 알고싶은 숫자들 -> 있으면 1, 없으면 0
-# 범위가 커서 시간복잡도가 O(N) 보다 작아야겠다.
+from sys import stdin
 
-from sys import stdin as s
-from collections import Counter, defaultdict
+N = int(stdin.readline())
+card_set = set(map(int, stdin.readline().split()))
+M = int(stdin.readline())
+numbers = list(map(int, stdin.readline().split()))
 
-N = int(s.readline())
-number_cards = s.readline().strip().split()
-M = int(s.readline())
-numbers = list(map(int, s.readline().strip().split()))
-
-card_dict = Counter(map(int, number_cards))
-number_dict = defaultdict(int)
-
+result = []
 for number in numbers:
-    number_dict[number] = card_dict[number]
+    result.append(1 if number in card_set else 0)
 
-print(*number_dict.values())
+print(*result)
