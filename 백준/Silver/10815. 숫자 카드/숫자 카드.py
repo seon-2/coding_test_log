e090bcb8cad9d692f27d44d79abfda5412931e02
seon-2 <73648391+seon-2@users.noreply.github.com>
2024-06-07 21:44:26 +0900

    [Silver V] Title: 숫자 카드, Time: 508 ms, Memory: 125620 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -6,7 +6,8 @@ M = int(input())
 numbers = list(map(int, input().split()))
 
 result = []
+
 for number in numbers:
     result.append(1 if number in card_set else 0)
 
-print(*result)
+print(' '.join(map(str, result)))
