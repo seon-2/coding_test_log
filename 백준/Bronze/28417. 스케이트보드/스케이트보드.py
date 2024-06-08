ffef9832dfc66de202b1f52353b97f505a3d9267
seon-2 <73648391+seon-2@users.noreply.github.com>
2024-06-08 13:31:38 +0900

    [Bronze III] Title: 스케이트보드, Time: 332 ms, Memory: 46260 KB -BaekjoonHub

--- 백준/Bronze/28417. 스케이트보드/스케이트보드.py
+++ 백준/Bronze/28417. 스케이트보드/스케이트보드.py
@@ -1,25 +1,20 @@
 # [0], [1] 중에서 큰거 + [2:7] 중에서 상위 2개 = 최종 점수
 
 from sys import stdin as s
-
-number_of_people = int(input())
-score_list = []
-final_score_list = []
-
-# 입력
-for _ in range(number_of_people):
-    score_list.append(list(map(int, s.readline().split())))
+from heapq import *
 
 
 def get_final_score(score):
     final_score = 0
     final_score += score[0] if score[0] > score[1] else score[1]
-    sorted_score = sorted(score[2:7])
-    final_score += sorted_score[-1] + sorted_score[-2]
+    heap = [-x for x in score[2:7]]
+    heapify(heap)
+    final_score -= heappop(heap)
+    final_score -= heap[0]
     return final_score
 
 
-for score in score_list:
-    final_score_list.append(get_final_score(score))
+number_of_people = int(s.readline())
+score_list = [list(map(int, s.readline().split())) for _ in range(number_of_people)]
 
-print(max(final_score_list))
+print(max(get_final_score(score) for score in score_list))
