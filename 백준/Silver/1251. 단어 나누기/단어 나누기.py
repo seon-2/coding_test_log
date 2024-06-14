8f49545937789e06e2646e60a5fd3ef347d283b6
seon-2 <73648391+seon-2@users.noreply.github.com>
2024-06-15 06:07:27 +0900

    [Silver V] Title: 단어 나누기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1251. 단어 나누기/단어 나누기.py
+++ 백준/Silver/1251. 단어 나누기/단어 나누기.py
@@ -1,13 +1,8 @@
 word = input().strip()
-divided_words = []
 new_words = []
-# 단어 세 부분으로 나누기
+
 for i in range(1, len(word)-1):
     for j in range(i+1, len(word)):
-        divided_words.append([word[:i], word[i:j], word[j:]])
-
-# 단어 뒤집어서 합치기
-for dw in divided_words:
-    new_words.append(dw[0][::-1] + dw[1][::-1] + dw[2][::-1])
+        new_words.append(word[:i][::-1] + word[i:j][::-1] + word[j:][::-1])
 
 print(sorted(new_words)[0])
