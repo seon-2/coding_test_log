62c916eff14e836ecd62dd3c1b73ee6acfb0cdd9
seon-2 <73648391+seon-2@users.noreply.github.com>
2024-06-17 09:32:34 +0900

    [Bronze I] Title: 적어도 대부분의 배수, Time: 44 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Bronze/1145. 적어도 대부분의 배수/적어도 대부분의 배수.py
+++ 백준/Bronze/1145. 적어도 대부분의 배수/적어도 대부분의 배수.py
@@ -1,12 +1,16 @@
-from sys import stdin as s
+from sys import stdin
 from itertools import combinations
 import math
 
-numbers = list(map(int, s.readline().strip().split()))
-combi = list(combinations(numbers, 3))
-result = []
+numbers = list(map(int, stdin.read().strip().split()))
 
-for combi_num in combi:
-    result.append(math.lcm(combi_num[0], combi_num[1], combi_num[2]))
+# 최소값 초기화
+min_lcm = float('inf')
 
-print(min(result))
+# 3개의 수를 조합해 최소 공배수를 계산하며 최소값 갱신
+for combi_num in combinations(numbers, 3):
+    lcm_value = math.lcm(*combi_num)
+    if lcm_value < min_lcm:
+        min_lcm = lcm_value
+
+print(min_lcm)
