f84b257b82139182ab69ddde6c6a418291e6e3ed
seon-2 <73648391+seon-2@users.noreply.github.com>
2024-07-24 00:58:39 +0900

    [level 1] Title: x만큼 간격이 있는 n개의 숫자, Time: 0.07 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.py
+++ 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.py
@@ -1,11 +1,2 @@
 def solution(x, n):
-    answer = []
-    end = n*x+1
-    if x == 0:
-        return [0] * n
-    elif x < 0:
-        end = n*x-1
-        
-    for i in range(x, end, x):
-        answer.append(i)
-    return answer
+    return [x * i for i in range(1, n + 1)]
