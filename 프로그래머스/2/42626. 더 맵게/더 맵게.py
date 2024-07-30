7c4f0189bbf129259ad355d925b19188a16ee61b
seon-2 <73648391+seon-2@users.noreply.github.com>
2024-07-30 22:47:33 +0900

    [level 2] Title: 더 맵게, Time: 1557.84 ms, Memory: 51.8 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,21 +1,16 @@
-from heapq import *
+from heapq import heapify, heappop, heappush
 
 def solution(scoville, K):
+    heapify(scoville)
+    mix_count = 0
     
-    if sum(scoville) == 0 and K > 0: # 모든 값이 0이라면 [0, 0, ...] 
-        count = -1 # 모든 음식의 스코빌을 K이상으로 만들 수 없음
-    else:
-        count = 0
-        heapify(scoville) # scoville 리스트를 힙으로 변경
+    while scoville[0] < K:
+        if len(scoville) < 2:
+            return -1
         
-        while scoville[0] < K:# 가장 작은 값이 K보다 작다면 더 섞어야 함
-            if len(scoville) < 2:
-                count = -1
-                break
-            
-            mixed_food = heappop(scoville) + heappop(scoville) * 2 # 섞고
-            count += 1
-            heappush(scoville, mixed_food) # 가진 음식에 추가
-
-    return count
-
+        first = heappop(scoville)
+        second = heappop(scoville)
+        heappush(scoville, first + second * 2)
+        mix_count += 1
+    
+    return mix_count
