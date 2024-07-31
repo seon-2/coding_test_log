49b28d1b909efa7c9ad66b2a0bcdb25643ed0b1a
seon-2 <73648391+seon-2@users.noreply.github.com>
2024-08-01 01:42:58 +0900

    [level 3] Title: 이중우선순위큐, Time: 65.39 ms, Memory: 18 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,24 +1,38 @@
 import heapq
 
 def solution(operations):
-    min_heap = [] # 최소힙
-    max_heap = [] # 최대힙
+    min_heap = []  # 최소 힙
+    max_heap = []  # 최대 힙
+    element_set = set()  # 현재 큐에 있는 요소를 추적하는 집합
     
     for op in operations:
-        command, data = map(str, op.split(" "))
-        number = int(data)
+        command, data = op.split()
         if command == "I":
-            heapq.heappush(min_heap, number)
-            heapq.heappush(max_heap, -number)
-        elif min_heap and max_heap and command == "D":
-            if number == 1: # 최댓값 삭제
-                max_value = heapq.heappop(max_heap)
-                min_heap.remove(-max_value) # 최소힙 리스트에서도 해당 
-            else: # 최소값 삭제
-                min_value = heapq.heappop(min_heap)
-                max_heap.remove(-min_value)   
+            num = int(data)
+            heapq.heappush(min_heap, num)
+            heapq.heappush(max_heap, -num)
+            element_set.add(num)
+        elif element_set and command == "D":
+            if data == "1":  # 최댓값 삭제
+                while max_heap and -max_heap[0] not in element_set:
+                    heapq.heappop(max_heap)
+                if max_heap:
+                    max_value = -heapq.heappop(max_heap)
+                    element_set.remove(max_value)
+            else:  # 최솟값 삭제
+                while min_heap and min_heap[0] not in element_set:
+                    heapq.heappop(min_heap)
+                if min_heap:
+                    min_value = heapq.heappop(min_heap)
+                    element_set.remove(min_value)
     
-    if min_heap and max_heap: # 비어있지 않으면
+    # 최종 정리
+    while max_heap and -max_heap[0] not in element_set:
+        heapq.heappop(max_heap)
+    while min_heap and min_heap[0] not in element_set:
+        heapq.heappop(min_heap)
+    
+    if element_set:
         return [-max_heap[0], min_heap[0]]
     else:
         return [0, 0]
