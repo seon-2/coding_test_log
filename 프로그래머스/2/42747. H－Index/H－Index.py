66d26c659e89c881167d75476b953943f57fc7b5
seon-2 <73648391+seon-2@users.noreply.github.com>
2024-08-02 22:13:22 +0900

    [level 2] Title: H-Index, Time: 0.18 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,19 +1,8 @@
-# 시간복잡도 개선
-
 def solution(citations):
     n = len(citations)
-    max_val = max(citations)
-    count = [0] * (max_val + 1)
-
-    # 각 인용 횟수에 해당하는 논문 수 계산
-    for c in citations:
-        count[c] += 1
-
-    total = 0
+    citations.sort(reverse=True)
     
-    for i in range(max_val, -1, -1):
-        total += count[i]
-        if total >= i:
-            return i
-
-    return 0
+    for i, citation in enumerate(citations, 1):
+        if citation < i:
+            return i - 1
+    return n
--- 프로그래머스/2/42747. H－Index/README.md
+++ 프로그래머스/2/42747. H－Index/README.md
@@ -1,14 +1,14 @@
 # [level 2] H-Index - 42747 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.01 ms
+메모리: 10.3 MB, 시간: 0.18 ms
 
 ### 구분
 
-코딩테스트 연습 > 정렬
+ > 
 
 ### 채점결과
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 29일 09:19:33
+2024년 08월 02일 22:13:20
 
 ### 문제 설명
 
